b083d5786288077dd9a09233f472459fc9d744ba
Git-Gfigueroa2912 <german_figueroa@unitec.edu>
2025-12-05 15:14:59 -0600

    Segundo Avance

--- src/lab8p1_germanfigueroa/Lab8P1_GermanFigueroa.java
+++ src/lab8p1_germanfigueroa/Lab8P1_GermanFigueroa.java
@@ -56,6 +56,42 @@ public class Lab8P1_GermanFigueroa {
         while(D < 3 || M < 3 || L < 3){
             int x = random.nextInt(5);
             int y = random.nextInt(5);
+            if(x == 0 && y == 0)continue;
+            if(x == 4 && y == 4)continue;
+            if(matriz[x][y] != ' ')continue;
+            
+            if(D < 3){
+                matriz[x][y] = 'D';
+                D++;
+            }else if(M < 3){
+                matriz[x][y] = 'M';
+                M++;
+            }else if(L < 3){
+                matriz[x][y] = 'L';
+                L++;
+            }
+        }
+        matriz[0][0] = 'E';
+        int pos = 0;
+        int tiempo = 0;
+        
+        while(tiempo < 60 && pos < 24){
+            int dado = random.nextInt(5)+1;
+            tiempo += random.nextInt(10)+1;
+            
+            int nuevaPos = pos + dado;
+            if(nuevaPos > 24) nuevaPos = 24;
+            
+            int fila = nuevaPos/5;
+            int columna = nuevaPos % 5;
+            char casilla = matriz[fila][columna];
+            
+            if(casilla == 'D'){
+                System.out.println("Mia te detuvo");
+            }else{
+                if (casilla == 'M') nuevaPos += 1;
+                if(casilla == 'L') nuevaPos += 3;
+            }
         }
     }
 }
